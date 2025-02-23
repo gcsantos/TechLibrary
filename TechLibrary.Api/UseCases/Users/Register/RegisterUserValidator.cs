789dbe3e6aa6b14c4208ef6cb8e8fc39bf7767b1
Guilherme Cintra <guilhermecs02@hotmail.com>
2025-02-23 10:25:38 -0300

    Fluxo de registro de um usuário

---
+++ TechLibrary.Api/UseCases/Users/Register/RegisterUserValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using TechLibrary.Communication.Request;
+
+namespace TechLibrary.Api.UseCases.Users.Register;
+
+public class RegisterUserValidator : AbstractValidator<RequestUserJson>
+{
+    public RegisterUserValidator()
+    {
+        RuleFor(request => request.Name).NotEmpty().WithMessage("Nome obrigatorio!");
+        RuleFor(request => request.Email).EmailAddress().WithMessage("Email invalido!");
+        RuleFor(request => request.Password).NotEmpty().WithMessage("Senha obrigatorio!");
+        When(request => string.IsNullOrEmpty(request.Password) == false, () =>
+        {
+            RuleFor(request => request.Password.Length).GreaterThanOrEqualTo(6).WithMessage("Senha não pode ser vazio e ser maior que 6 caractere!");
+        });
+    }
+}
